15f1ae02f2d8f621641ed184b0e834f1311f3aff
Jaxon2020 <61923016+Jaxon2020@users.noreply.github.com>
2021-11-04 15:09:05 -0500

    Update app.py
    test

--- app.py
+++ app.py
@@ -28,6 +28,14 @@ def md5(string):
     print(hashjson)
     return hashjson
 
+
+
+@app.route("/keyval", methods=["POST"])
+
+def keyval():
+
+    
+
 @app.route("/factorial/<int:factor>")
 def factorial(factor):
     i = factor
