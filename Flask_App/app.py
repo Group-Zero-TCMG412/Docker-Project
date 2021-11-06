3fb7f67aa77799e635e4e78f5e001e179538f479
Jaxon2020 <61923016+Jaxon2020@users.noreply.github.com>
2021-11-05 21:59:14 -0500

    Update app.py

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -10,18 +10,10 @@ import hashlib
 # Connect to Redis
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port='6379')
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+r = redis.Redis(
+    host='redis',
+    port= 6379,
+    password= '')
 
 @app.route("/count")
 def index():
