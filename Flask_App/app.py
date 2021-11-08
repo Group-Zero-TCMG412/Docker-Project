9cdbb27b8e0bd0702037af557da257e339947fad
JoshNwton <90420290+JoshNwton@users.noreply.github.com>
2021-11-08 16:25:06 -0600

    code update

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -75,21 +75,16 @@ def keyvalGETDELETE(string):
         else:
             KeyDict = {'Key': string, 'value': redis_client.get(string), 'command': 'READ key/value pair', 'result': False, 'error': 'key does not exist'}
         return KeyDict
-    elif request.method == 'DELETE':
-<<<<<<< Updated upstream
-        if redis_client.exists(string) == 1;
+        
+     elif request.method == 'DELETE':
+        if redis_client.exists(string) == 1
+            KeyDict = {'Key': string, 'value': redis_client.del(string), 'command': 'DELETE' {key}, 'result': True}
             KeyDict = {'Key': string, 'value': redis_client.del(string), 'command': 'DELETE' {key}, 'result': True, 'error': 'None'}
             return KeyDict
         else:
+            KeyDict = {'Key': string, 'value': , 'command' : 
             KeyDict = {'Key': string, 'value': redis_client.get(string), 'command' : 'DELETE' {key}, 'result': False, 'error': 'Unable to delete, key not found'}
             return KeyDict
-=======
-        pass
-        #if redis_client.exists(string) == 1;
-            #KeyDict = {'Key': string, 'value': redis_client.del(string), 'command': 'DELETE' {key}, 'result': True}
-        #else:
-            #KeyDict = {'Key': string, 'value': , 'command' : 
->>>>>>> Stashed changes
     
 
 @app.route("/md5/<string>", methods=["GET","POST"])
